ea80cf3c0fc3b4e90a842ae0bdd7007514e6842d
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-25 00:15:33 +0300

    5/6 complete

--- T5.py
+++ T5.py
@@ -21,13 +21,28 @@
 # naula = y = 32b
 # luoti = b = 13.3g
 
-x = 20 * y
-y = 32 * b
-b = 13.3
-
 print ("Syötä leivisköjen, naulojen ja luotien määrä.")
-x = float(input("Leivisköjen kpl määrä: "))
-y = float(input("Naulojen kpl määrä: "))
-b = float(input("Luotien kpl määrä: "))
+value1 = float(input("Leivisköjen kpl määrä: "))
+value2 = float(input("Naulojen kpl määrä: "))
+value3 = float(input("Luotien kpl määrä: "))
 
+b = 13.3
+y = 32 * b
+x = 20 * y
 
+leivi = value1 * x
+naula = value2 * y
+luoti = value3 * b
+total = leivi + naula + luoti
+
+#tiedosto testi.py mallina käyttäen -> print(f"Kolmion ala on: {total:10.2f}")
+# muokkasin -> 10.1f
+
+print (f"Leivisköjen massa on: + {leivi:10.1f}" + "g")
+print (f"Naulojen massa on: + {naula:10.1f}" + "g")
+print (f"Luotien massa on: + {luoti:10.1f}" + "g")
+# print (f"Massa yht on:  + {total:10.2f}" + "g")
+# kg + g ilmoittamisessa ongelmia, ainoa ratkaisu jonka löysin ->
+kilograms = int(total // 1000)
+grams = total % 1000
+print(f"Massa yht on: {kilograms} kg ja {grams:.1f}g")
