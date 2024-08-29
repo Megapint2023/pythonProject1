91e1bccb8219286e8d30f72d6a651debe59d1fc6
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-29 10:42:06 +0300

    Module 3 assignement 1 complete

--- M3T1.py
+++ M3T1.py
@@ -3,4 +3,13 @@
 # montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen,
 # jos sen pituus on alle 37 cm.
 
-print (vastaus)
+
+kuhasize = float(input("Syötä kuhan pituus (cm):"))
+
+if kuhasize < 37:
+    alipaino = int (37 - kuhasize)
+    print ("Alle 37cm pituinen kuha on alamittainen, joten laske se takaisin.")
+    print ("Kuhasi on " + str(alipaino) + "cm liian lyhyt")
+
+if kuhasize >= 37:
+    print ("Kuhan voi nostaa!")
