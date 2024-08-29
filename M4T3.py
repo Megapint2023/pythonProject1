e315bfcc9d726e048ec649b7b4cced0108d301cb
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-29 17:04:41 +0300

    Module 4 assignement 3 (in progress)

--- M4T3.py
+++ M4T3.py
@@ -1,2 +1,23 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+
+# Eli käytännössä ohjelma jatkuvasti pyytää lukuarvoja, kunnes käyttäjä painaa enter.
+
+print ("Ohjelma valitsee syötetyistä arvoista pienimmän ja suurimman.")
+print ("Lopeta ohjelma painamalla enter.")
+
+pienin = None
+suurin = None
+
+while True:
+    number = float (input ("Syö tä uusi luku: "))
+    if number == "":
+        break
+    if number < pienin:
+        pienin = number
+    if number > suurin:
+        suurin = number
+
+print (str(pienin))
+print (str(suurin))
+
