69b96996557ea01924e41d874713ae96f85d56ab
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-29 12:51:15 +0300

    Module 3 assignement 3 complete

--- M3T3.py
+++ M3T3.py
@@ -4,4 +4,19 @@
 #Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
 #Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
 
-print (vastaus)
+print ("Tarkista onko hemoglobiinisi normaali / korkea ->")
+genre = input("Syötä sukupuolesi (kirjain) M/N (mies/nainen): ")
+hemoglobine = float(input("Syötä hemoglobiiniarvo arvo väliltä -> 117-175(nainen) tai 134-195 (mies): "))
+
+if genre == "M" and hemoglobine > 134 and hemoglobine < 195:
+    print ("Normaali hemoglobiiniarvo.")
+elif genre == "M" and hemoglobine > 195:
+    print("Korkea homeglobiini!")
+elif genre == "M" and hemoglobine < 134:
+    print("Virhe, tarkista syötetty arvo.")
+elif genre == "N" and hemoglobine > 117 and hemoglobine < 175:
+    print ("Normaali hemoglobiiniarvo.")
+elif genre == "N" and hemoglobine > 175:
+    print("Korkea homeglobiini!")
+elif genre == "N" and hemoglobine < 117:
+    print("Virhe, tarkista syötetty arvo.")
