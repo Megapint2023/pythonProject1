a5ea778abfcca32466e64ea8ec3436d550c20f51
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-29 11:06:57 +0300

    Module 3 assignement 2 complete

--- M3T2.py
+++ M3T2.py
@@ -8,4 +8,24 @@
 #C on ikkunaton hytti autokannen alapuolella.
 #Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
 
-print (vastaus)
+print ("Laivan hyttiluokat: (LUX/A/B/C)")
+luokka = (input("Tarkista hyttisi sijainti syöttämällä hyttiluokkasi: "))
+
+if luokka == "LUX":
+    print ("Parvekkeellinen hytti eläkannella.")
+
+elif luokka == "A":
+    print("Ikkunallinen hytti autokannen yläpuolella.")
+
+elif luokka == "B":
+    print("Ikkunaton hytti autokannen yläpuolella.")
+
+elif luokka == "C":
+    print("Ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka.")
+
+# HUOM.  ->
+    # 1 Pythonissa on käytettävä == merkkejä tarkistaakseen "vastaako arvoa", koska pelkkä = käytetään arvojen laatimiseen.
+    # 2 "" on lisättävä Pythonissa, jotta se osaa lukea ne stringeinä.
