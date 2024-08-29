5bed514fa7d0187e278adc6bb96e6a395525217c
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-29 09:51:18 +0300

    Added module 3 assignements

--- T1.py
+++ T1.py
@@ -1,4 +1,4 @@
-#Kirjoita ohjelma, joka kysyy nimesi ja sen jälkeen tervehtii sinua omalla nimelläsi.
+#Kirjoita ohjelma, joka kysyy käyttäjän nimen ja sen jälkeen tervehtii sillä.
 #Esimerkkejä:
 #Jos syötät nimeksesi Viivi, ohjelma tervehtii sinua sanoin Terve, Viivi!
 #Jos syötät nimeksesi Ahmed, ohjelma tervehtii sinua sanoin Terve, Ahmed!
