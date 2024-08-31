bfaa97e2152fd2b8b270513c7ffc047161992b13
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-31 19:46:06 +0300

    Module 4 assignement 4 testing

--- M4T4.py
+++ M4T4.py
@@ -2,3 +2,17 @@
 # Kone arvuuttelee lukua pelaajalta siihen asti, kunnes tämä arvaa oikein.
 # Kunkin arvauksen jälkeen ohjelma tulostaa tekstin Liian suuri arvaus, Liian pieni arvaus tai Oikein.
 # Huomaa, että tietokone ei saa vaihtaa lukuaan arvauskertojen välissä.
+
+print ("Arvaa numero väliltä 1-10")
+import random
+secret_number = random.randint(1, 10)
+
+while True:
+    arvaus = input ("Syötä uusi numero: ")
+    if arvaus == secret_number:
+        print ("Oikein!")
+        break
+    elif arvaus < secret_number:
+        print ("Väärin, luku on suurempi. Arvaa uudestaan!")
+    elif arvaus > secret_number:
+        print ("Väärin, luku on pienempi. Arvaa uudestaan!")
