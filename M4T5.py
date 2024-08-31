5e66a5a89ea06949201b747bff356df355732660
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-31 19:56:45 +0300

    Module 4 assignement 5 progress

--- M4T5.py
+++ M4T5.py
@@ -3,3 +3,12 @@
 # Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
+from ctypes import pythonapi
+
+#Tee while silmukka joka tekee 5 kierrosta tai break mikäli tiedot matchää
+#Tunnus ja salasana on määritetty valmiiksi.
+
+print ("Kirjaudu oikeilla tiedoilla.")
+
+u_name = "python"
+u_pass = "rules"
