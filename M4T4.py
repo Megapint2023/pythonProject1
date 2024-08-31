cf3fe88eb469a520ebd0ac07b7b30c6b8a074cba
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-08-31 19:50:06 +0300

    Module 4 assignement 4 complete

--- M4T4.py
+++ M4T4.py
@@ -8,10 +8,13 @@ import random
 secret_number = random.randint(1, 10)
 
 while True:
-    arvaus = input ("Syötä uusi numero: ")
+    arvaus = input("Syötä uusi numero: ")
+    arvaus = int(arvaus)
+
     if arvaus == secret_number:
         print ("Oikein!")
         break
+
     elif arvaus < secret_number:
         print ("Väärin, luku on suurempi. Arvaa uudestaan!")
     elif arvaus > secret_number:
