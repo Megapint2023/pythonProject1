4baa3038494400ab7b32e64f3f50d99c143cb100
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-01 11:02:46 +0300

    Module 4 assignement 5 complete

--- M4T5.py
+++ M4T5.py
@@ -3,13 +3,27 @@
 # Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
-from ctypes import pythonapi
 
 #Tee while silmukka joka tekee 5 kierrosta tai break mikäli tiedot matchää
 #Tunnus ja salasana on määritetty valmiiksi.
 
-print ("Kirjaudu oikeilla tiedoilla.")
 
-u_name = "python"
-u_pass = "rules"
+print ("Kirjaudu sisään tunnuksella ja salasanalla.")
 
+username = "python"
+password = "rules"
+
+kirjautumisyritys = 0
+while kirjautumisyritys <= 5:
+    username_input = input("Syötä käyttäjätunnus:")
+    password_input = input("Syötä salasana:")
+
+    if username_input == username and password_input == password:
+        print("Great success... commander.")
+        break
+    else:
+        kirjautumisyritys = kirjautumisyritys + 1
+        if kirjautumisyritys < 5:
+            print("Väärä tunnus tai salasana, yritä uudelleen.")
+        else:
+            print("Liian monta yritystä.")
