0065b8564b1e66fd3405f55a2b0b612fbf264b79
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-01 18:57:53 +0300

    Module 5 assignement 3 complete

--- M5T3.py
+++ M5T3.py
@@ -18,12 +18,14 @@ while True:
 if luku == 1:
     alkuluku = False
 else:
-    for x in range(2, luku):
-        if luku % x == 0:
+    for x in range(2, luku):  # muuttujaan "x" tallettuu joka ikinen arvo 2 - syötetty arvo
+        if luku % x == 0:     # Mikäli yhdenkään jakojäännökseksi tulee "0"  -> alkuluku = false
             alkuluku = False
             break
+# range(2, luku) toimii koska se tarkistaa kaikki luvut paitsi viimeisen (Python hard coded)
 
 if alkuluku:
     print (str(luku) + " on alkuluku!")
 else:
     print(str(luku) + " ei ole alkuluku.")
+# viimeinen ehtolause printtaa tiedon tuloksen mukaan.
