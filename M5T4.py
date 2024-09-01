e42bc7f0b69b34641b5f0db26a6d2e93c3121659
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-01 20:21:25 +0300

    Module 5 assignement 4 progress

--- M5T4.py
+++ M5T4.py
@@ -2,3 +2,14 @@
 # (käytä for-toistorakennetta nimien kysymiseen) ja tallentaa ne listarakenteeseen.
 # Lopuksi ohjelma tulostaa kaupunkien nimet yksi kerrallaan allekkain samassa järjestyksessä kuin ne syötettiin.
 # käytä for-toistorakennetta nimien kysymiseen ja for/in toistorakennetta niiden läpikäymiseen.
+
+print("Syötä viiden kaupungin nimet.")
+
+kaupungit = ["","","","",""]
+
+kaupunki[0] = input("Ensimmäisen kaupunki: ")
+kaupunki[1] = input("Toisen kaupunki:")
+kaupunki3 = input("Kolmannen kaupunki:")
+kaupunki4 = input("Neljännen kaupunki:")
+kaupunki5 = input("Viidennen kaupunki:")
+
