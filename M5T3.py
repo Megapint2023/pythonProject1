957bb4b9c7706f2f899ef6c55583e37889342903
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-01 18:22:54 +0300

    Module 5 assignement 3 progress

--- M5T3.py
+++ M5T3.py
@@ -3,3 +3,18 @@
 # Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten,
 # että jako menee tasan.
 # Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
+
+
+alkuluku = None
+
+while True:
+    luku = input("Syötä luku: ")
+    luku = int(luku)
+    if luku < 1:
+        print("Virheellinen positiivinen kokonaisluku.")
+    else:
+        break
+
+if luku == luku and luku % 1 == 0:
+    luku = alkuluku
+    print (str(alkuluku) + " on alkuluku!")
