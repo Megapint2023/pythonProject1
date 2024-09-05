aa305665e9d3a9162d935d6bef0e27fa60d03350
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-05 17:05:50 +0300

    Module 6 assignement 4 progress

--- M6T3.py
+++ M6T3.py
@@ -5,14 +5,20 @@
 # kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
 
+print ("Ohjelma muuttaa gallonit litroiksi.")
+print ("Saat lopetettua ohjelman antamalla negatiivisen arvon.")
 
 def galloneistlitroiks():
     while True:
        gallons = float("Nontako gallonia muutetaan litroiksi")
+       if gallons < 0:
+           print ("Syötit negatiivisen arvon -> ohjelma loppu.")
+           break
+       else:
        litra = gallons * 3.785
        litra = float(litra)
        return litra
-
+       print (litra)
 
 
 galloneistlitroiks()
