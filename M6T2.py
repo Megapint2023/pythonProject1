33c39dfa7927ef44d0aae373d514427563f714c9
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-05 16:48:29 +0300

    Module 6 assignement 2 complete

--- M6T2.py
+++ M6T2.py
@@ -2,3 +2,29 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
 # joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+# TAHKO = nopansivu, esim 6 tahkoinen noppa = 1-6 arvoa sisältävä
+#
+
+import random
+
+def parametriton():
+ #!!!!! tämä "eka" while loop lisäsin, jotta tahkomäärä on järkevä eli max 100 !!!!
+    while True:
+        tahkot = input("Syötä kuinka monta tahkoa (eli sivua) nopalla on: ")
+        tahkot = int(tahkot)
+        if tahkot < 1 or tahkot > 100:
+            print ("Älä keuli... Syötä järkevä arvo 1-100 välillä: ")
+        else:
+            break
+
+    while True:
+        nopanheitto = random.randint (1, tahkot )
+        if nopanheitto == tahkot:
+            print(str(nopanheitto) + " -> Osuit maksimiin!")
+            break
+        else:
+            print ("Heitit: " + str(nopanheitto) + ":n")
+
+parametriton()
+
