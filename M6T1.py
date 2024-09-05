bc009ebbd671f609899ba5dcc50cadb6c4c7c838
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-05 15:50:46 +0300

    Module 6 assignement 1 progress

--- M6T1.py
+++ M6T1.py
@@ -1,3 +1,17 @@
-# Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
+# Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen
+# nopan silmäluvun väliltä 1..6.
 # Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+#parametriton eli ilman numero arvoa
+# jos laitan funtkioon muuttujan se ei ole numero arvo
+# eli voin tehdä näin ->
+
+def parametriton(noppa):
+    numero = noppa
+    return numero
+
+parametriton()
+import random
+noppa = random.randint(1, 6)
+print(parametriton)
