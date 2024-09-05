46358f6bdc05806397629bd9a3338dbe195aed64
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-05 18:36:52 +0300

    Module 6 assignement 4 progresss

--- M6T4.py
+++ M6T4.py
@@ -1,3 +1,24 @@
 # Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 # Ohjelma palauttaa listassa olevien lukujen summan.
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+
+
+print("Ohjelma palauttaa listassa olevien numeroiden summan.")
+numerolista = ["5", "11", "2", "18", "24", "19"]
+
+def numero(numerolista):
+    for x in numerolista:
+        numerolista = numerolista +1
+    return (numerolista)
+numerot = int(numerot)
+numerolista.append(numerolista)
+
+numeroidensumma = numerolista
+
+print(numerot)
+
+# täytyy tehdä lista ja kirjoittaa sinne valmiiksi arvot
+# Sitten luoda looppi joka menee listan läpi ja poimii sen arvot eri listalle?
+# sitten laskee arvojen summan yhteen ja printtaa sen.
+# lisäksi voisin vielä printata näkyviin arvot joiden summan ohejlma on laskenut
+# Fun
