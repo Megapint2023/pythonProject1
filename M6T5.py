efa9d930ee3f947f60bc7e750a20cf9dc9241d50
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 04:53:56 +0300

    Module 6 assignement 6 complete

--- M6T5.py
+++ M6T5.py
@@ -7,7 +7,7 @@
 def alikaks(parilliset):
     print(parilliset)
 
-def aliyks(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20):
+def aliyks(kaiqqinum):
     print(kaiqqinum)
     alikaks(parilliset)
 
