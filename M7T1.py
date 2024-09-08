dc8fe616684f909d676d9b89307e77d3d38aaae1
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 22:17:05 +0300

    Module 7 assignement 1 progress

--- M7T1.py
+++ M7T1.py
@@ -1,3 +1,18 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron, jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talvi).
-# Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+# Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron.
+# Sen jälkeen ohjelma tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talvi).
+# -> Parametri 3X3
+# Tallenna ohjelmassasi kuukausia vastaavat vuodenajat !!!!merkkijonoina monikkotietorakenteeseen!!!!.
 # Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
+
+tammikuu = 2
+helmikuu = 3
+maaliskuu = 4
+huhtikuu = 5
+toukokuu = 6
+kesäkuu = 7
+heinakuu = 8
+elokuu = 9
+syyskuu = 10
+lokakuu = 11
+marraskuu = 12
+joulukuu = 1 # tähtäväannon mukaan
