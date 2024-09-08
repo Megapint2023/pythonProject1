9116fb65280bf00b02d6d47e696efda1ef9653bd
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 08:11:33 +0300

    Module 6 assignement 6 progress

--- M6T6.py
+++ M6T6.py
@@ -6,25 +6,45 @@
 # Pääohjelma kysyy käyttäjältä: kahden pizzan halkaisijat ja hinnat
 # Lopuksi ilmoittaa kummasta saa enemmän vastinetta rahalle.
 # Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
-# Ykköshinta = lopullinen kaiken kattava hinta = eli myyntihinta (tässä tap).
+# (Ykköshinta = lopullinen hinta)
+
+
+#ympyrän pinta-ala A = 𝜋 * d
+# Laske -> euro per neliömetri pizzaa
+# value = A / hinta
+
+
+import math
+
+def pizza1(value1):
+    value1 = 0
+    A1 = PII * d1
+    A1 = float(A1)
+    value1 = A1 / hinta1
+    return value1
+    print ("Ensimmäisen pizzan rahavastine = " + str(value1))
+
+def pizza1(value2):
+    value2 = 0
+    A2 = PII * d1
+    A2 = float(A2)
+    return A2
+    value2 = A2 / hinta2 # €/m²
+    print(value2)
 
 
 
-def pizza1(halkaisija):
-    halkaisija = 2 * d
-    return halkaisija
 
-def pizza2(halkaisija):
-
-
-print ("Ohjelma vertaa kummasta pizzasta sai enemmän vastinetta rahoille")
+print ("Ohjelma vertaa kummasta pizzasta sait enemmän vastinetta rahoillesi.")
 
 d1 = input("Syötä ensimmäisen pizzan halkaisija senttimetreinä: ")
 d1 = float(d1)
+hinta1 = input("Syötä ensimmäisen pizzan hinta: ")
+hinta1 = float(hinta1)
 d2 = input("Syötä toisen pizzan halkaisija senttimetreinä: ")
 d2 = float(d2)
-hinta1 = input("Syötä ensimmäisen pizzan hinta: ")
-hinta = float(hinta1)
-hinta1 = input("Syötä toisen pizzan hinta: ")
-hinta = float(hinta)
+hinta2 = input("Syötä toisen pizzan hinta: ")
+hinta2 = float(hinta2)
 
+pizza1(value)
+pizza2(value)
