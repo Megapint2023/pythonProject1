67c35a73fb35befa5ba58ead5e41b6fa2339a9b5
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 23:59:08 +0300

    Module 7 assingment 7 progress

--- M7T1.py
+++ M7T1.py
@@ -8,18 +8,38 @@
 
 print("Ohjema ilmoittaa kk vastaavan vuodenajan.")
 
-kuukaudet = ("joulukuu", "tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu",
+kk = ("joulukuu", "tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu",
               "heinakuu", "elokuu", "syyskuu", "lokakuu", "marraskuu")
-("talviaika", "talviaika", "talviaika", "talviaika", kevat, kevat, kevat, kevat, kesa, kesa, kesa, kesa, syksy, syksy, sysky, syksy ) = hedelmät
+("talviaika", "talviaika", "talviaika", "talviaika", "kevataika", "kevataika", "kevataika",
+ "kevataika", "kesäaika", "kesäaika", "kesäaika", "kesäaika", "syksyaika", "syksyaika", "syksyaika", "syksyaika" ) = hedelmät
+
+kuukaudet = {"Viivi":"050-1234567",
+           "Ahmed":"040-1112223",
+           "Pekka":"050-7654321"}
+
+numerot["Olga"] = "050-1011012"
+numerot["Mary"] = "0401-2132139"
+
+print (numerot)
+
+nimi = input("Anna nimi: ")
+if nimi in numerot:
+    print (f"Henkilön {nimi} puhelinnumero on {numerot[nimi]}.")
+
+def ali2()
+    if kk = talvi
+    print("kk taulukosta siihen vuodenaikaan kuuluvat kk:det")
 
 def vuodenajat(kknro)
     if kknro = 1 or 2 or 3
-        print ("talvi"")
+        kknro = talvi
     elif kknro = 4 or 5 or 6
-        print ("kevät")
+         kknro = kevat
     elif kknro = 7 or 8 or 9
+         kknro = kesa
         print ("Kesä beibe!")
     elif kknro = 10 or 11 or 12
+         kknro = kevät
         print ("kevät")
 
 talvi = 1, 2, 3
