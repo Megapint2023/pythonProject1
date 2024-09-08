373a69ec6be7211ac97fe739f1a97b7ab7123477
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-09 02:45:45 +0300

    Module 7 assingment 1 complete

--- M7T1.py
+++ M7T1.py
@@ -30,28 +30,24 @@ def kuukausi(kknro):
     rivi = kknro + 1
     if rivi > 12:
         rivi = 1
-    elif:
-        print(f"Syöttämäsi kuukausi oli: {kkinfo[rivi]} .")
+    print(f"KAUUKAUSI: {kkinfo[rivi]}")
     vuodenaika(kknro)
 
 # tässä kerrotaan käyttäjälle vuodenajan lisäksi muut kuukaudet jotka siihen kuuluvat
 def vuodenaika(kknro):
-    if kknro == 1 or 2 or 3:
-         print ("Vuodenaika: Talvi - joulukuu, tammikuu ja maaliskuu")
-    elif kknro == 4 or 5 or 6:
-         print ("Vuodenaika: Kevät -  huhtikuu, toukokuu ja kesäkuu")
-    elif kknro == 7 or 8 or 9:
-         print ("Vuodenaika: Kesä -  heinäkuu, elokuu ja syyskuu")
-    elif kknro == 10 or 11 or 12:
-         print ("Vuodenaika: Syksy - joulukuu, tammikuu ja maaliskuu")
-         return kknro
+    if kknro > 0 and kknro <= 3:
+        print ("Vuodenaika:Talvi (joulukuu, tammikuu ja helmikuu)")
+    elif kknro >= 4 and kknro <= 6:
+        print ("Vuodenaika:Kevät (maaliskuu, huhtikuu ja toukokuu)")
+    elif kknro >= 7 and kknro <= 9:
+        print ("Vuodenaika:Kesä  (kesäkuu, heinäkuu ja elokuu)")
+    elif kknro >= 10 and kknro <= 12:
+        print ("Vuodenaika:Syksy (syyskuu, lokakuu ja marraskuu)")
 
-kknro = input("Syötä kuukauden numero 1-12: ") # käyttäjänhän ei tarvitse tietää, että joulukuu = "1"
+kknro = int(input("Syötä kuukauden numero 1-12: ")) # käyttäjänhän ei tarvitse tietää, että joulukuu = "1"
 kknro = int(kknro)
 
 kuukausi(kknro)
-vuodenaika(kknro)
-
 
 
 
