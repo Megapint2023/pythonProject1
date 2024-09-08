96c64498ba90c605640dd9170fb51af50d0bb2a0
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-09 00:30:33 +0300

    Module 7 assingment 7 progress

--- M7T1.py
+++ M7T1.py
@@ -8,6 +8,30 @@
 
 print("Ohjema ilmoittaa kk vastaavan vuodenajan.")
 
+
+kk = ("joulukuu", "tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu",
+              "heinakuu", "elokuu", "syyskuu", "lokakuu", "marraskuu")
+
+def kkinfo(i)
+    if kknro = talvi
+
+    print("kk [3]")
+
+def vuodenaika(kknro)
+    if kknro = 1 or 2 or 3
+         print ("Tämä on talviaikaa.")
+    elif kknro = 4 or 5 or 6
+         print ("Tämä on kevättä.")
+    elif kknro = 7 or 8 or 9
+         print ("Tämä on kesäaikaa.")
+    elif kknro = 10 or 11 or 12
+         print ("Tämä on syksyä.")
+
+#talvi = 1, 2, 3
+#kevat = 4, 5, 6
+#kesa = 7, 8, 9
+#syksy = 10, 11, 12
+
 kkinfo = {"Joulukuu":"talviaika",
            "Tammikuu":"talviaika",
            "Helmikuu":"talviaika",
@@ -20,35 +44,23 @@ kkinfo = {"Joulukuu":"talviaika",
              "Lokakuu":"talviaika",
              "Marraskuu":"talviaika",
              }
+vuodenaika()
 
-def vuodenaika(kknro)
-    if kknro = 1 or 2 or 3
-         print ("Syötetty kk ja vuodenaika ovat: ")
-    elif kknro = 4 or 5 or 6
-         print ("Syötetty kk ja vuodenaika ovat: ")
-    elif kknro = 7 or 8 or 9
-         print ("Syötetty kk ja vuodenaika ovat: ")
-    elif kknro = 10 or 11 or 12
-         print ("Syötetty kk ja vuodenaika ovat: ")
+
 
 kknro = input("Syötä kuukauden numero 1-12: ") # käyttäjänhän ei tarvitse tietää, että joulukuu = "1"
 kknro = int(kknro)
 # Nyt -> aliohjelma, joka määrittää vuodenajan
 
-vuodenaika()
+kkinfo()
 
 
 
 
-#kk1 = ("joulukuu", "tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu",
-              "heinakuu", "elokuu", "syyskuu", "lokakuu", "marraskuu")
-def ali2()
-    if kknro = talvi
-
-    print("kk [3]")
 
 
-talvi = 1, 2, 3
-kevat = 4, 5, 6
-kesa = 7, 8, 9
-syksy = 10, 11, 12
+
+
+
+
+
