48de32d6c316aa864f22a66be3bdd9406ad1d33e
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 04:45:35 +0300

    Module 6 assignement 5 + 6 progress

--- M6T5.py
+++ M6T5.py
@@ -4,19 +4,19 @@
 # Tulosta molemmat listat
 
 
-def alikaks():
+def alikaks(parilliset):
     print(parilliset)
 
-def aliyks():
-    print(kaikkiluvut)
-    return alikaks()
+def aliyks(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20):
+    print(kaiqqinum)
+    alikaks(parilliset)
 
 print ("Ohjelma tulostaa 2x listaa. Kaikki / vain parittomat numerot.")
 
-kaikkiluvut = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+kaiqqinum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 parilliset = [2,4,6,8,10,12,14,16,18,20]
 
 
-aliyks()
+aliyks(kaiqqinum)
 
 
