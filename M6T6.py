81e6de148139693969fff2200e66622cb05785a5
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-08 05:51:42 +0300

    Module 6 assignement 6 progress

--- M6T6.py
+++ M6T6.py
@@ -1,6 +1,30 @@
-# Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä
-# sekä pizzan hinnan euroina.
-# #Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri.
-# Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilmoittaa,
-# kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
+# Kirjoita funktio, joka saa parametrinä:
+# 1. pizzan halkaisijan senttimetreinä
+# 2. Sen hinnan euroina.
+# 3. palauttaa pizzan yksikköhinnan euroina per neliömetri.
+
+# Pääohjelma kysyy käyttäjältä: kahden pizzan halkaisijat ja hinnat
+# Lopuksi ilmoittaa kummasta saa enemmän vastinetta rahalle.
 # Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+# Ykköshinta = lopullinen kaiken kattava hinta = eli myyntihinta (tässä tap).
+
+
+
+def pizza1(halkaisija):
+    halkaisija = 2 * d
+    return halkaisija
+
+def pizza2(halkaisija):
+
+
+print ("Ohjelma vertaa kummasta pizzasta sai enemmän vastinetta rahoille")
+
+d1 = input("Syötä ensimmäisen pizzan halkaisija senttimetreinä: ")
+d1 = float(d1)
+d2 = input("Syötä toisen pizzan halkaisija senttimetreinä: ")
+d2 = float(d2)
+hinta1 = input("Syötä ensimmäisen pizzan hinta: ")
+hinta = float(hinta1)
+hinta1 = input("Syötä toisen pizzan hinta: ")
+hinta = float(hinta)
+
