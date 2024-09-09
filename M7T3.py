db0956c48e342eb09405b7aa8b58be73086fee83
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-09 06:54:22 +0300

    Module 7 assingment 3 progress

--- M7T3.py
+++ M7T3.py
@@ -1,4 +1,5 @@
-#Kirjoita ohjelma lentoasematietojen hakemiseksi ja tallentamiseksi.
+# OHJELMA - LENTOKENTTÄHAKU TAI LISÄYS -
+
 # Ohjelma kysyy käyttäjältä, haluaako tämä syöttää uuden lentoaseman,
 # hakea jo syötetyn lentoaseman tiedot vai lopettaa.
 
@@ -10,12 +11,29 @@
 # kunnes hän haluaa lopettaa. (ICAO-koodi on lentoaseman yksilöivä tunniste.
 # Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.)
 
-# -> Eli inputin sijaan tarttee jonkun monivalinta systeemin?...
 
+# -> Eli inputin sijaan tarttee jonkun monivalinta systeemin?...
 print ("OHJELMA: VALITSE TOIMINTO")
 print ("[1] = LENTOKENTTÄHAKU [ICAO-koodi]")
 print ("[2] = LISÄÄ PUUUTTUVA LENTOKENTTÄ")
 print ("[ENTER] = LOPETA OHJELMA")
+
+while True:
+    uus = 0
+    valinta = input("Syötä valinta:")
+    if valinta == "":
+        print ("Ohjelma loppu.")
+        break
+    if valinta == 1:
+       valinta = int(haku)
+       print("*LENTOKENTTÄHAKU*")
+    if valinta == 2:
+       valinta = int(lisa)
+  else:
+       print("Virheellinen valinta! Syötä luku 1, 2 tai paina pelkästään enteriä.")
+       return = uus
+
+
 
 
 # Get Flight info
