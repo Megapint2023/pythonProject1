cfdc41b912f6ed0e814b85dc3d177eddd48dabf4
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-09 03:54:35 +0300

    Module 7 assingment 2 progress

--- M7T2.py
+++ M7T2.py
@@ -2,5 +2,46 @@
 # kunnes käyttäjä syöttää tyhjän merkkijonon.
 # Kunkin nimen syöttämisen jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan,
 # syötettiinkö nimi ensimmäistä kertaa.
-# Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä.
+# Lopuksi ohjelma printtaa sisällön sattumanvaraisesti ->
 # Käytä joukkotietorakennetta nimien tallentamiseen.
+
+#joukkotietorakenne
+# Python: "set" =
+# Tallentaa tietoa kuten muutkin listat listat, mutta
+
+
+allnimet = {"" : ""}
+nimet = set()
+nimet.add("Viivi")
+print(nimet)
+
+nimi = input("Lisää uusi nimi: (lopeta painamalla 'enter': :")
+
+
+if nimi in allnimet:
+    print(f"Henkilö{nimi}") {puhelinnumero[]}
+else nimi = allnimet +1+
+
+
+
+for i in nimiaukko:  # python automaattisesti menee numerolistan läpi
+    summa = summa + i
+    print("- " + str(i))
+return summa
+
+minisetti = {"nimi1", "nimi2", "nimi3"}
+print(minisetti)
+
+minisetti.add("Dominion")
+print(minisetti)
+
+minisetti.remove("Shakki")
+print(minisetti)
+
+minisetti.add("Cluedo")
+print(minisetti)
+
+for p in minisetti:
+    print(p)
+
+
