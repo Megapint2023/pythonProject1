731fc8d87a8cb24999b37cabcb4260bb61708e13
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-13 16:03:47 +0300

    Module 8 assingment 1 progress

--- M8T1.py
+++ M8T1.py
@@ -1,3 +1,9 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin.
-# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
+# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja
+# sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
 # ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
+
+print ("Ohjelma hakee ICAO-koodilla lentokentän tiedot.")
+
+icao = str("ICAO koodi:")
+
