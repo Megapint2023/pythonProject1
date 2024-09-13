7a0b6432a32934e65f109701f94753c37a7b1f5f
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-13 21:30:37 +0300

    Module 8 assignment 2 progress

--- M8T2.py
+++ M8T2.py
@@ -2,3 +2,31 @@
 # olevien lentokenttien lukumäärät tyypeittäin.
 # Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä,
 # että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
+
+
+import mysql.connector
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='megapint',
+         password='wine',
+         autocommit=True
+         )
+
+def hae_tiedot(icao):
+    sql = f"SELECT name, municipality FROM airport WHERE ident = '{icao}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        for rivi in tulos:
+            print(f"KAUPUNKI: {rivi[0]}, KUNTA: {rivi[1]}")
+    else:
+        print("Kyseisellä ICAO-koodilla ei löytynyt tietoja.")
+    kursori.close()
+
+print ("Ohjelma hakee ICAO-koodilla lentokentän tiedot.")
+icao = input(str("ICAO koodi:"))
+hae_tiedot(icao)
