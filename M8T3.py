b8d415cb8f64945c8dac4944ef003056ee593e45
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-16 00:50:19 +0300

    Module 8 assignment 3 progress

--- M8T3.py
+++ M8T3.py
@@ -4,3 +4,33 @@
 # Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
 # Asenna kirjasto valitsemalla View / Tool Windows / Python Packages.
 # Kirjoita hakukenttään geopy ja vie asennus loppuun.
+
+
+import mysql.connector
+# from collections import Counter # LISÄTTY. Python moduuli joka tehty erinäisen datan laskemiseen.
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='megapint',
+         password='wine',
+         autocommit=True
+         )
+
+def calculate_distance(icao1, icao2):
+    sql = f"SELECT latitude_deg, longtitude_deg FROM aiport WHERE ident = '{icao1, icao2}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+
+
+
+
+
+print()
+
+print ("Ohjelma laskee kahden lentokentän etäisyyden ICAO koodien aculla.")
+icao1 = input("Syötä ensimmäisen kentän ICAO:")
+icao2 = input("Syötä toisen kentän ICAO:")
+
+calculate_distance()
