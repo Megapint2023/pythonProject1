47d473815595b0216bfa9d4fcc32e5aa67c31c8c
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-22 12:21:02 +0200

    Module 9 assignment 2 progress

--- M9T2.py
+++ M9T2.py
@@ -18,17 +18,29 @@ class Auto: # tämä class
         self.matka = 0
 
     def kiihdytys(self):
-        for i in range(10):
-            self.nopeus += 10
-            if self.nopeus > self.huippunopeus:
-                self.nopeus = self.huippunopeus
-            elif self.nopeus < 0:
-                self.nopeus = 0
+        self.nopeus += 10 # nopeus nousee +10km/h per metodin käyttö
+        if self.nopeus > self.huippunopeus: # poikkeus huippunopeuteen saavuttaesssa
+            self.nopeus = self.huippunopeus
+        elif self.nopeus < 0:
+            self.nopeus = 0
             print(f"Nopeus: {self.nopeus} km/h")
 
 auto = Auto("ABC-123", 142) # tämä on olio/object
 print (f"Auton tiedot -> Rekisterinumero: {auto.rekisteri}, Huippunopeus {auto.huippunopeus} km/h, Nopeus nyt: {auto.nopeus} km/h.")  # olien ominaisuuksien tulos
 
+# kiihdytys 1
 print (f"Auto kiihdyttää...")
 for i in range(3):
     auto.kiihdytys()
+print(f"Auton nopeus nyt: {auto.nopeus} km/h")
+# kiihdytys 2
+print (f"Auto kiihdyttää...")
+for i in range(5):
+    auto.kiihdytys()
+print(f"Auton nopeus nyt: {auto.nopeus} km/h")
+# kiihdytys 3
+print (f"Auto kiihdyttää...")
+for i in range(7):
+    auto.kiihdytys()
+print(f"Auton nopeus nyt: {auto.nopeus} km/h")
+
