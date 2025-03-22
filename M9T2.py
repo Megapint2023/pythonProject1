69edfc45a0326d44da50905faaba28d9d4985fce
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-22 11:03:55 +0200

    Module 9 assignment 2 progress

--- M9T2.py
+++ M9T2.py
@@ -8,3 +8,15 @@
 # Tee sitten hätäjarrutus määräämällä nopeuden muutos -200 km/h ja tulosta uusi nopeus.
 # Kuljettua matkaa ei tarvitse vielä päivittää.
 # 19.03.2025
+
+class Auto: # tämä class
+    def __init__(self, rekisteri, huippunopeus): # tämä alustaja
+        # ominaisuudet
+        self.rekisteri = rekisteri
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+auto = Auto("ABC-123", 142) # tämä on olio/object
+
+print (f"{auto.rekisteri} {auto.huippunopeus:d} {auto.nopeus:d} {auto.matka:d}")  # olien ominaisuuksien tulos
