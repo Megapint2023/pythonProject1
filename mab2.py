53dffc3a51b7b3aae1be78462d5ddfe9b82d7c40
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-26 12:09:15 +0200

    mab1,mab2, mab3 assingments complete

---
+++ mab2.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = 137.7
+b = 62.3
+
+a_rad = np.radians(a)
+b_rad = np.radians(b)
+
+print(a_rad)
+print(b_rad)
--- testi.py
+++ testi.py
@@ -1,17 +1,43 @@
-#class Koira:
-#    pass
-#
-#koira = Koira()
-#koira.nimi = "Rekku"
-#koira.syntymavuosi = 2022
-#
-#print (f"{koira.nimi:s} on syntynyt vuonna {koira.syntymavuosi:d}.")
+class Työntekijä:
 
-class Koira:
-    def __init__(self, nimi, syntymävuosi):
-        self.nimi = nimi
-        self.syntymävuosi = syntymävuosi
+    työntekijöiden_lukumäärä = 0
 
-koira = Koira("Rekku", 2022)
+    def __init__(self, etunimi, sukunimi):
+        Työntekijä.työntekijöiden_lukumäärä += 1
+        self.työntekijänumero = Työntekijä.työntekijöiden_lukumäärä
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
 
-print (f"{koira.nimi:s} on syntynyt vuonna {koira.syntymävuosi:d}." )
+    def tulosta_tiedot(self):
+        print(f"{self.työntekijänumero}: {self.etunimi} {self.sukunimi}")
+
+class Tuntipalkkainen(Työntekijä):
+    def __init__(self, etunimi, sukunimi, tuntipalkka):
+        super().__init__(etunimi, sukunimi)
+        self.tuntipalkka = tuntipalkka
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f' - Tuntipalkka on {self.tuntipalkka}')
+
+class Kuukausipalkkainen(Työntekijä):
+    def __init__(self, etunimi, sukunimi, kuukausipalkka):
+        super().__init__(etunimi, sukunimi)
+        self.kuukausipalkka = kuukausipalkka
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f' - Kuukausipalkka on {self.kuukausipalkka}')
+
+
+
+
+työntekijät = []
+työntekijät.append(Työntekijä("Viivi", "Virta"))
+työntekijät.append(Työntekijä("Ahmed", "Habib"))
+
+työntekijät.append(Tuntipalkkainen('Pertti', 'Kaski', 50.0))
+työntekijät.append(Kuukausipalkkainen('Iina', 'Kekkiläinen', 5000.0))
+
+for t in työntekijät:
+    t.tulosta_tiedot()
