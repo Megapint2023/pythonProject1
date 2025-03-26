80eaa7718ae4891ec471abd50dcfd7f24622c9a0
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-26 09:21:46 +0200

    Module 9 assignment 3 progress

--- M9T3.py
+++ M9T3.py
@@ -1,4 +1,4 @@
-#Laajenna ohjelmaa siten, että mukana on kulje-metodi, joka saa parametrinaan tuntimäärän.
+# Laajenna ohjelmaa siten, että mukana on kulje-metodi, joka saa parametrinaan tuntimäärän.
 # Metodi kasvattaa kuljettua matkaa sen verran kuin auto on tasaisella vauhdilla annetussa tuntimäärässä edennyt.
 # Esimerkki: auto-olion tämänhetkinen kuljettu matka on 2000 km. Nopeus on 60 km/h.
 # Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaan 2090 km.
@@ -9,43 +9,34 @@ class Auto: # tämä class
         # ominaisuudet
         self.rekisteri = rekisteri
         self.huippunopeus = huippunopeus
-        self.nopeus = 0
-        self.matka = 0
+        self.nopeus = 60
+        self.matka = 2000
 
-    def kiihdytys(self):
-        self.nopeus += 10 # nopeus nousee +10km/h per metodin käyttö
-        if self.nopeus > self.huippunopeus: # poikkeus huippunopeuteen saavuttaesssa
-            self.nopeus = self.huippunopeus
-        elif self.nopeus < 0:
-            self.nopeus = 0
-            print(f"Nopeus: {self.nopeus} km/h")
+    #def kiihdytys(self):
+    #    self.nopeus += 10 # nopeus nousee +10km/h per metodin käyttö
+    #    if self.nopeus > self.huippunopeus: # poikkeus huippunopeuteen saavuttaesssa
+    #        self.nopeus = self.huippunopeus
+    #    elif self.nopeus < 0:
+    #        self.nopeus = 0
+    #        print(f"Nopeus: {self.nopeus} km/h")
+#
+    #def jarrutus(self):
+    #    self.nopeus -= 10
+    #    if self.nopeus < 0:
+    #        self.nopeus = 0
 
-    def jarrutus(self):
-        self.nopeus -= 10
-        if self.nopeus < 0:
-            self.nopeus = 0
+    def kulje(self, matka, aika):
+        self.matka = matka
+        self.aika = aika
 
 auto = Auto("ABC-123", 142) # tämä on olio/object
 print (f"Auton tiedot -> Rekisterinumero: {auto.rekisteri}, Huippunopeus {auto.huippunopeus} km/h, Nopeus nyt: {auto.nopeus} km/h.")  # olien ominaisuuksien tulos
 
-# kiihdytys 1
-print (f"Auto kiihdyttää...")
-for i in range(3):
-    auto.kiihdytys()
-print(f"Auton uusi nopeus: {auto.nopeus} km/h.")
-# kiihdytys 2
-print (f"Auto kiihdyttää...")
-for i in range(5):
-    auto.kiihdytys()
-print(f"Auton uusi nopeus: {auto.nopeus} km/h.")
-# kiihdytys 3
-print (f"Auto kiihdyttää...")
-for i in range(7):
-    auto.kiihdytys()
+print (f"Auton kilometrilukema: {auto.matka} km.")
+
 print(f"Auton uusi nopeus: {auto.nopeus} km/h.")
 
-# Äkkijarrutus!
-print (f"Auto tekee hätäjarrutuksen!")
-for i in range(20):
-    auto.jarrutus()
-print(f"Auton uusi nopeus: {auto.nopeus} km/h.")
+print (f"Matkan jälkeen uusi kilometrilukema on.")
+
+
+
