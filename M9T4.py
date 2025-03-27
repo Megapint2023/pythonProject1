5f439b29f4b94c7bcce9f39b8bdd5adfb340227e
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-27 16:24:21 +0200

    Module 9 assignment 4 progress

--- M9T4.py
+++ M9T4.py
@@ -18,5 +18,32 @@ class Auto: # tämä class
         # ominaisuudet
         self.rekisteri = rekisteri
         self.huippunopeus = huippunopeus
-        self.nopeus = 60
-        self.matka = 2000
+        self.nopeus = 0
+        self.matka = 0
+
+    def kiihdytys(self):
+        self.nopeus += 10 # nopeus nousee +10km/h per metodin käyttö
+        if self.nopeus > self.huippunopeus: # poikkeus huippunopeuteen saavuttaesssa
+            self.nopeus = self.huippunopeus
+        elif self.nopeus < 0:
+            self.nopeus = 0
+            print(f"Nopeus: {self.nopeus} km/h")
+
+    def jarrutus(self):
+        self.nopeus -= 10
+        if self.nopeus < 0:
+            self.nopeus = 0
+
+    def kulje(self, aika):
+        self.matka += self.nopeus * aika
+
+auto = Auto("ABC-123", 142) # tämä on olio/object
+print (f"Auton tiedot -> Rekisterinumero: {auto.rekisteri}, Huippunopeus {auto.huippunopeus} km/h, Nopeus nyt: {auto.nopeus} km/h.")  # olien ominaisuuksien tulos
+
+print (f"Auton kilometrilukema: {auto.matka} km.")
+
+print(f"Auton uusi nopeus: {auto.nopeus} km/h.")
+
+auto.kulje(1.5)
+
+print (f"Matkan jälkeen uusi kilometrilukema on: {auto.matka} km.")
