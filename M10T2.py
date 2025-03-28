b27562e30ed7482b277b735f19eea258630d64d8
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-28 10:23:32 +0200

    Module 10 assignment 1 complete

--- M10T2.py
+++ M10T2.py
@@ -1,6 +1,32 @@
-#Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan.
+# Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan.
 # Talon alustajaparametreina annetaan alimman ja ylimmän kerroksen numero sekä hissien lukumäärä.
 # Talon luonnin yhteydessä talo luo tarvittavan määrän hissejä.
 # Hissien lista tallennetaan talon ominaisuutena. Kirjoita taloon metodi aja_hissiä,
 # joka saa parametreinaan hissin numeron ja kohdekerroksen.
 # Kirjoita pääohjelmaan lauseet talon luomiseksi ja talon hisseillä ajelemiseksi.
+
+class Hissi:
+    def __init__(self, alin, ylin): # alustaja
+        # ominaisuudet
+        self.kerros = 0
+        self.ylin = ylin
+        self.alin = alin
+
+    def liiku_ylos(self): #metodi
+        if self.kerros < self.ylin:
+            self.kerros += 1
+            print(f"Saavuit kerrokseen: {self.kerros}.")
+
+    def liiku_alas(self): #metodi
+        if self.kerros > self.alin:
+            self.kerros -= 1
+            print(f"Saavuit kerrokseen: {self.kerros}.")
+
+    def h(self): #metodi
+        while self.kerros < self.ylin:
+            self.liiku_ylos()
+        while self.kerros > self.alin:
+            self.liiku_alas()
+
+hissi = Hissi(0, 5)
+hissi.h()
