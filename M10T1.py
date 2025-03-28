cee95a33a2e1054163bc93ea7be85de686d15490
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-28 09:38:16 +0200

    Module 10 assignment 1 progress

--- M10T1.py
+++ M10T1.py
@@ -10,22 +10,32 @@
 # sen jälkeen takaisin alimpaan kerrokseen.
 
 class Hissi:
-    def __init__(self, alin, ylin, kerros):
-        self.kerros = kerros
-        self.ylin = 10
+    def __init__(self, alin, ylin, kerros): # alustaja
+        # ominaisuudet
+        self.kerros = 0
+        self.ylin = 5
         self.alin = 0
 
+    def liiku_ylos(self):
+        self.kerros = self.kerros + 1
 
-    def h(self):
+    def h(self): #metodi
         self.kerros = 5
 
+    def liiku(self):
+        muutos = random.randint(, 15)
+        self.kerros += muutos
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        elif self.nopeus < 0:
+            self.nopeus = 0
 
-
-
+    def kulje(self, aika):
+        self.matka += self.nopeus * aika
     def kerros(self):
         self.kerros = (0, 10)
-        if self.kerros <= 0:
-            self.kerros = alin
+        if self.kerros == 5:
+            break
         elif self.kerros >= 10
             self.kerros =ylin
 
