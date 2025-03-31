e6a5fcd41ce07e897ff204a4ec7948e06a8d2747
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-31 17:52:32 +0300

    Module 12 assignment 1 progress

--- M12T1.py
+++ M12T1.py
@@ -1,3 +1,12 @@
 #Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle.
 # Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/.
 # Käyttäjälle on näytettävä pelkkä vitsin teksti.
+
+import requests
+
+hakusana = input("Anna hakusana: ")
+
+# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+vastaus = requests.get(pyyntö).json()
+print(vastaus)
