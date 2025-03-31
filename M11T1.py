b8298ac1349eb77f8b59e7f8e7541046f1c410f5
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-31 09:50:02 +0300

    Module 11 assignment 1 progress

--- M11T1.py
+++ M11T1.py
@@ -5,15 +5,63 @@
 # Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua).
 # Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
 
-class Julkaisu:
-    def __init__(self, tyyppi, nimi, tuottaja, sivumäärä):
-        self.tyyppi = tyyppi
-        self.nimi = nimi
-        self.tuottaja = tuottaja
-        self.sivumäärä = sivumäärä
-
-    def generate(self):
-        print(f"{Julkaisu.tyyppi()}, {Julkaisu.nimi()}, {Julkaisu.tuottaja()}, {Julkaisu.sivumäärä()}.")
 
 
-julkaisu1 = Julkaisu("Kirja", "Meikäläiset", "Ella Bella", 135)
+class Työntekijä:
+
+    työntekijöiden_lukumäärä = 0
+
+    def __init__(self, etunimi, sukunimi):
+        Työntekijä.työntekijöiden_lukumäärä += 1
+        self.työntekijänumero = Työntekijä.työntekijöiden_lukumäärä
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+
+    def tulosta_tiedot(self):
+        print(f"{self.työntekijänumero}: {self.etunimi} {self.sukunimi}")
+
+class Tuntipalkkainen(Työntekijä):
+    def __init__(self, etunimi, sukunimi, tuntipalkka):
+        super().__init__(etunimi, sukunimi)
+        self.tuntipalkka = tuntipalkka
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f' - Tuntipalkka on {self.tuntipalkka}')
+
+class Kuukausipalkkainen(Työntekijä):
+    def __init__(self, etunimi, sukunimi, kuukausipalkka):
+        super().__init__(etunimi, sukunimi)
+        self.kuukausipalkka = kuukausipalkka
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f' - Kuukausipalkka on {self.kuukausipalkka}')
+
+
+
+
+työntekijät = []
+työntekijät.append(Työntekijä("Viivi", "Virta"))
+työntekijät.append(Työntekijä("Ahmed", "Habib"))
+
+työntekijät.append(Tuntipalkkainen('Pertti', 'Kaski', 50.0))
+työntekijät.append(Kuukausipalkkainen('Iina', 'Kekkiläinen', 5000.0))
+
+for t in työntekijät:
+    t.tulosta_tiedot()
+
+
+
+#class Julkaisu:
+#    def __init__(self, tyyppi, nimi, tuottaja, sivumäärä):
+#        self.tyyppi = tyyppi
+#        self.nimi = nimi
+#        self.tuottaja = tuottaja
+#        self.sivumäärä = sivumäärä
+#
+#    def generate(self):
+#        print(f"{Julkaisu.tyyppi()}, {Julkaisu.nimi()}, {Julkaisu.tuottaja()}, {Julkaisu.sivumäärä()}.")
+#
+#
+#julkaisu1.generate("Kirja", "Meikäläiset", "Ella Bella", 135)
