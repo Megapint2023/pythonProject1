7e5ad35b78a5e134dd059f1571ade051f0fc3370
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-31 22:17:02 +0300

    Module 12 assignment 2 progress

--- M12T2.py
+++ M12T2.py
@@ -1,7 +1,21 @@
-#Tutustu avoimeen OpenWeather-säärajapintaan: https://openweathermap.org/api.
+# Tutustu avoimeen OpenWeather-säärajapintaan: https://openweathermap.org/api.
 # Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen
 # ja tulostaa sitä vastaavan säätilan tekstin sekä lämpötilan Celsius-asteina.
 # Perehdy rajapinnan dokumentaatioon riittävästi.
 # Palveluun rekisteröityminen on tarpeen,
 # jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
 # Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
+
+
+import requests
+import json
+
+hakusana = input("Anna kaupunki: ")
+
+pyyntö = "https://openweathermap.org/api" + hakusana
+vastaus = requests.get(pyyntö)
+data = vastaus.json()
+
+print(vastaus)
+print(data)
+print(json.dumps(data, indent=2))
