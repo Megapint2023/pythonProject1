35e3925b1b9e392575d5684a6bf840e50f8a3f0f
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-03-31 20:55:45 +0300

    Module 12 assignment 1 complete

--- M12T1.py
+++ M12T1.py
@@ -2,11 +2,35 @@
 # Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/.
 # Käyttäjälle on näytettävä pelkkä vitsin teksti.
 
+#import requests
+#import json
+#
+#hakusana = 'cats'
+#osoite = "https://api.tvmaze.com/search/shows?q=" + hakusana
+#    vastaus = requests.get(osoite)
+#    data = vastaus.json()
+#
+#    print(vastaus)
+#    print(data)
+#    print(json.dumps(data, indent=2))
+#
+#    for tv_show in data:
+#        #print(show)
+#       # print(['show'])
+#        print(tv_show['show']['name'])
+#        show = tv_show['show']
+#        name = show['name']
+#        print(show['name'])
+#        print(name)
+
+
 import requests
+import json
 
-hakusana = input("Anna hakusana: ")
+osoite = "https://api.chucknorris.io/jokes/random"
+vastaus = requests.get(osoite)
+data = vastaus.json()
 
-# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
-pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
-vastaus = requests.get(pyyntö).json()
 print(vastaus)
+print(data)
+print("Chuck Norris vitsi:", data["value"])
