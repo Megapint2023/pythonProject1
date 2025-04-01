27b54983beb9025a721e0b76bf21fd0ac6be2c73
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-01 15:27:48 +0300

    Module 12 assignment 2 progress

--- M12T2.py
+++ M12T2.py
@@ -5,14 +5,19 @@
 # Palveluun rekisteröityminen on tarpeen,
 # jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
 # Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
+# U: playtime1945
+# P: strong
+# @: metropolia
+# API:
 
 
 import requests
 import json
 
-hakusana = input("Anna kaupunki: ")
+hakusana = input("Anna paikkakunta: ")
+api_key = "543cd516ab7095dab8faac0cf1a3731d"
 
-pyyntö = "https://openweathermap.org/api" + hakusana
+pyyntö = "https://api.openweathermap.org/data/2.5/weather?q={hakusana}&appid={api_key}"
 vastaus = requests.get(pyyntö)
 data = vastaus.json()
 
