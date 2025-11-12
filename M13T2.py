cf400266c4b5d4e9d191aa2133dd364338bc7fb3
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-12 16:36:37 +0200

    Module 13 assingment 1 complete

--- M13T2.py
+++ M13T2.py
@@ -1,5 +1,42 @@
-#Toteuta taustapalvelu, joka palauttaa annettua lentokentän ICAO-koodia vastaavan lentokentän nimen
+# Toteuta taustapalvelu, joka palauttaa annettua lentokentän ICAO-koodia vastaavan lentokentän nimen
 # ja kaupungin JSON-muodossa.
 # Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta.
 # Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
 # Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
+
+from flask import Flask, Response
+import json
+
+app = Flask(__name__)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+@app.route('/alkuluku/<int:luku>')
+def alkuluku(luku):
+    tulos = is_prime(luku)
+    tilakoodi = 200
+    vastaus = {
+        "Number": luku,
+        "isPrime": tulos
+    }
+
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus = {
+        "status": 404,
+        "teksti": "Virheellinen päätepiste"
+    }
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=404, mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
